110dbdacda243ccf98f8ccad30d058cf2dad3dba
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-01-29 21:54:43 +0200

    more quests

---
+++ scripts/copy_quests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -r config/ftbquests
+cp -r "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/config/ftbquests/" config/
---
+++ scripts/copy_scripts.sh
@@ -0,0 +1,5 @@
+#/usr/bin/env bash
+
+rm -r "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/server_scripts"
+cp -r kubejs/server_scripts/ "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/"
+
