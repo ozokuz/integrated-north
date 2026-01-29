fe4a8280b58b83ee87260be97077cda3cc33a14b
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-01-29 22:40:09 +0200

    fix missing portable storage interface recipe

--- kubejs/server_scripts/fixes.js
+++ kubejs/server_scripts/fixes.js
@@ -1,3 +1,4 @@
 ServerEvents.recipes((event) => {
   event.stonecutting("6x create:brass_tunnel", "createages:brass_machine");
+  event.stonecutting("2x create:portable_storage_interface", "createages:andesite_machine");
 });
