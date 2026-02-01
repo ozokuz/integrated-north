eb9fdb00346b406ea27354f07e75586bfeae6a39
Ozoku <19756856+ozokuz@users.noreply.github.com>
2026-02-01 20:27:00 +0200

    make script also copy client scripts

--- scripts/copy_scripts.sh
+++ scripts/copy_scripts.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env bash
 
 rm -r "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/server_scripts"
+rm -r "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/client_scripts"
 cp -r kubejs/server_scripts/ "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/"
+cp -r kubejs/client_scripts/ "$HOME/.local/share/PrismLauncher/instances/Integrated North Dev/minecraft/kubejs/"
 
